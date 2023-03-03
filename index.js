0be382b31ca82b3ec92c39305359d169fe1aa9cd
bossSteve03 <stotev0@gmail.com>
2023-03-03 12:57:42 +0000

    latest update

--- index.js
+++ index.js
@@ -8,12 +8,53 @@ console.log(rules)
 //create board
 let gameboard = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
 let isOver = false;
+function computerInputFunction() {
+    let wrongChoice = true
+
+while(wrongChoice = true){
+    const computerInput = () => {
+    let c = Math.floor(Math.random()*9)+1
+    return c
+}
+switch(computerInput()){
+    case '1':
+        tl === '-' ? ((gameboard[0][0] = 'O'), wrongChoice = false) : wrongChoice = true;
+        break;
+    case '2':
+        t === '-' ? ((gameboard[0][1] = 'O'), wrongChoice = false) : wrongChoice = true
+        break;
+    case '3':
+        tr === '-' ? ((gameboard[0][2] = 'O'), wrongChoice = false) : wrongChoice = true
+        break;
+    case '4':
+        ml === '-' ? ((gameboard[1][0] = 'O'), wrongChoice = false) : wrongChoice = true
+        break;
+    case '5':
+        m === '-' ? ((gameboard[1][1] = 'O'), wrongChoice = false) : wrongChoice = true
+        break;
+    case '6':
+        mr === '-' ? ((gameboard[1][2] = 'O'), wrongChoice = false) : wrongChoice = true
+        break;
+    case '7':
+        bl === '-' ? ((gameboard[2][0] = 'O'), wrongChoice = false) : wrongChoice = true
+        break;
+    case '8':
+        b === '-' ? ((gameboard[2][1] = 'O'), wrongChoice = false) : wrongChoice = true
+        break;
+    case '9':
+        br === '-' ? ((gameboard[2][2] = 'O'), wrongChoice = false) : wrongChoice = true
+        break;
+    }
+    
+}
+}
 
 // Create the slots
 
 
 
 //function to check if there a winner
+
 const printBoard = () => {
     gameboard.forEach(el => {console.log(el)})
 }
@@ -22,16 +63,69 @@ printBoard()
 
 while(!isOver) {
 
+    const computerWin = () => {if(tl == 'O' && t == 'O' && tr == 'O') {
+        console.log("The computer won!");
+        isOver = true;    
+    } else if(ml == 'O' && m == 'O' && mr == 'O') {
+        console.log("The computer won!");
+        isOver = true;
+    } else if(bl == 'O' && b == 'O' && br == 'O') {
+        console.log("The computer won!");
+        isOver = true;
+    } else if(tl == 'O' && ml == 'O' && bl == 'O') {
+        console.log("The computer won!");
+        isOver = true;
+    } else if(t == 'O' && m == 'O' && b == 'O') {
+        console.log("The computer won!");
+        isOver = true;
+    } else if(tr == 'O' && mr == 'O' && br == 'O') {
+        console.log("The computer won!");
+        isOver = true;
+    } else if(tl == 'O' && m == 'O' && br == 'O') {
+        console.log("The computer won!");
+        isOver = true;
+    } else if(bl == 'O' && m == 'O' && tr == 'O') {
+        console.log("The computer won!");
+        isOver = true;
+    }}
+
+    const win = () => {
+        if((tl == 'X') && (t == 'X') && (tr == 'X')) {
+            console.log("You won!");
+            isOver = true;
+            return    
+        } else if(ml == 'X' && m == 'X' && mr == 'X') {
+            console.log("You won!");
+            isOver = true;
+            return
+        } else if(bl == 'X' && b == 'X' && br == 'X') {
+            console.log("You won!");
+            isOver = true;
+            return
+        } else if(tl == 'X' && ml == 'X' && bl == 'X') {
+            console.log("You won!");
+            isOver = true;
+            return
+        } else if((t == 'X') && (m == 'X') && (b == 'X')) {
+            console.log("You won!");
+            isOver = true;
+            return
+        } else if(tr == 'X' && mr == 'X' && br == 'X') {
+            console.log("You won!");
+            isOver = true;
+            return
+        } else if(tl == 'X' && m == 'X' && br == 'X') {
+            console.log("You won!");
+            isOver = true;
+            return
+        } else if(bl == 'X' && m == 'X' && tr == 'X') {
+            console.log("You won!");
+            isOver = true;
+            return
+        } else isOver = false; return
+    }
     
-
-//log empty board
-
-
-//prompt user input
-
-let userInput = prompt('Enter the slot you\'d like? ').toLowerCase()
-
-let tl = gameboard[0][0]
+    let tl = gameboard[0][0]
     let t = gameboard[0][1]
     let tr = gameboard[0][2]
     let ml = gameboard[1][0]
@@ -40,108 +134,58 @@ let tl = gameboard[0][0]
     let bl = gameboard[2][0]
     let b = gameboard[2][1]
     let br = gameboard[2][2]
+    
+
+//log empty board
+
+
+//prompt user input
+win()
+computerWin()
+if(isOver == true){
+    break
+}
+let userInput = prompt('Enter the slot you\'d like? ').toLowerCase()
 
 //is the spot available + error handling
 switch (userInput) {
     case 'top left':
-        tl === '-' ? gameboard[0][0] = 'X' : console.log("Choose again.")
+        tl === '-' ? (gameboard[0][0] = 'X') : console.log("Choose again.")
         break;
     case 'top':
-        t === '-' ? gameboard[0][1] = 'X' : console.log("Choose again.")
+        t === '-' ? (gameboard[0][1] = 'X') : console.log("Choose again.")
         break;
-    case 'top left':
-        tr === '-' ? gameboard[0][2] = 'X' : console.log("Choose again.")
+    case 'top right':
+        tr === '-' ? (gameboard[0][2] = 'X') : console.log("Choose again.")
         break;
     case 'middle left':
-        ml === '-' ? gameboard[1][0] = 'X' : console.log("Choose again.")
+        ml === '-' ? (gameboard[1][0] = 'X') : console.log("Choose again.")
         break;
     case 'middle':
-        m === '-' ? gameboard[1][1] = 'X' : console.log("Choose again.")
+        m === '-' ? (gameboard[1][1] = 'X') : console.log("Choose again.")
         break;
     case 'middle right':
-        mr === '-' ? gameboard[1][2] = 'X' : console.log("Choose again.")
+        mr === '-' ? (gameboard[1][2] = 'X') : console.log("Choose again.")
         break;
     case 'bottom left':
-        bl === '-' ? gameboard[2][0] = 'X' : console.log("Choose again.")
+        bl === '-' ? (gameboard[2][0] = 'X') : console.log("Choose again.")
         break;
     case 'bottom':
-        b === '-' ? gameboard[2][1] = 'X' : console.log("Choose again.")
+        b === '-' ? (gameboard[2][1] = 'X') : console.log("Choose again.")
         break;
     case 'bottom right':
-        br === '-' ? gameboard[2][2] = 'X' : console.log("Choose again.")
+        br === '-' ? (gameboard[2][2] = 'X') : console.log("Choose again.")
         break;
     default:
         console.log("Choose again.");
 }
 
-
-
 printBoard()
 
-console.log(t)
-
-//is the user a winner?
-
-if((tl == 'X') && (t == 'X') && (tr == 'X')) {
-    console.log("You won!");
-    isOver = true;    
-} else if(ml == 'X' && m == 'X' && mr == 'X') {
-    console.log("You won!");
-    isOver = true;
-} else if(bl == 'X' && b == 'X' && br == 'X') {
-    console.log("You won!");
-    isOver = true;
-} else if(tl == 'X' && ml == 'X' && bl == 'X') {
-    console.log("You won!");
-    isOver = true;
-} else if((t == 'X') && (m == 'X') && (b == 'X')) {
-    console.log("You won!");
-    isOver = true;
-} else if(tr == 'X' && mr == 'X' && br == 'X') {
-    console.log("You won!");
-    isOver = true;
-} else if(tl == 'X' && m == 'X' && br == 'X') {
-    console.log("You won!");
-    isOver = true;
-} else if(bl == 'X' && m == 'X' && tr == 'X') {
-    console.log("You won!");
-    isOver = true;
-} 
-
-//is the computer a winner?
-
-if(tl == 'O' && t == 'O' && tr == 'O') {
-    console.log("The computer won!");
-    isOver = true;    
-} else if(ml == 'O' && m == 'O' && mr == 'O') {
-    console.log("The computer won!");
-    isOver = true;
-} else if(bl == 'O' && b == 'O' && br == 'O') {
-    console.log("The computer won!");
-    isOver = true;
-} else if(tl == 'O' && ml == 'O' && bl == 'O') {
-    console.log("The computer won!");
-    isOver = true;
-} else if(t == 'O' && m == 'O' && b == 'O') {
-    console.log("The computer won!");
-    isOver = true;
-} else if(tr == 'O' && mr == 'O' && br == 'O') {
-    console.log("The computer won!");
-    isOver = true;
-} else if(tl == 'O' && m == 'O' && br == 'O') {
-    console.log("The computer won!");
-    isOver = true;
-} else if(bl == 'O' && m == 'O' && tr == 'O') {
-    console.log("The computer won!");
-    isOver = true;
-}
-
-
-//log user's input
-
-
 //computer chooses (random)
+computerInputFunction();
 
+printBoard()
 
 //is the spot available
 
